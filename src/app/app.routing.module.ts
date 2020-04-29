280cb72aba7bbde16da15dea08b7dcf1846747e0
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-04-29 12:45:39 -0300

    Added logged user route guard & new user creation

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,13 +6,20 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { SignInComponent } from "./home/signin/signin.component";
+import { AuthGuard } from "./core/auth/auth.guard";
+import { SignUpComponent } from "./home/signup/signup.component";
 
 
 const routes: Routes = [
 
   {
     path: '',
-    component: SignInComponent
+    component: SignInComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
   },
   {
     path: 'user/:userName',
