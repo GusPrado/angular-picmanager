956df93484cd7072a3b14c9ef75c81f3c9be6535
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-04-30 12:00:11 -0300

    Added footer & photo upload method

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -25,5 +25,15 @@ export class PhotoService {
 
   }
 
+  upload(description: string, allowComments: boolean, file: File) {
+
+    const formData = new FormData()
+    formData.append('description', description)
+    formData.append('allowComments', allowComments ? 'true' : 'false')
+    formData.append('imageFile', file)
+
+    return this.http.post(API + '/photos/upload', formData)
+  }
+
 
 }
