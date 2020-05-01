541a709cda3b51a057f420369d8616ef1aa874b2
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-04-30 21:33:12 -0300

    Added photo comments feature

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -4,14 +4,13 @@ import { PhotoService } from "../photo/photo.service";
 import { Photo } from "../photo/photo";
 import { Observable } from "rxjs";
 
-
 @Component({
   templateUrl: './photo-details.component.html',
-  styleUrls: ['./photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit {
 
   photo$: Observable<Photo>
+  photoId: number
 
   constructor(
     private route: ActivatedRoute,
@@ -19,8 +18,8 @@ export class PhotoDetailsComponent implements OnInit {
     ){}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params.photoId
-    this.photo$ = this.photoService.findById(id)
+    this.photoId = this.route.snapshot.params.photoId
+    this.photo$ = this.photoService.findById(this.photoId)
 
   }
 
